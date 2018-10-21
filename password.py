83ec29566857bdc7142d0c340fd36acaf5e0670f
Bence <pscheng1203@gmail.com>
2018-10-22 00:30:40 +0800

    update description when x = 0

--- password.py
+++ password.py
@@ -2,9 +2,13 @@ password = 'a123456'
 x = 3
 while x > 0:
 	pw = input('請輸入密碼')
+	x = x - 1
 	if pw == password:
 		print('登入成功')
 		break
 	else:
-		x = x -1
-		print('密碼錯誤, 你還有', x, '次機會')
+		print('密碼錯誤')
+		if x > 0:
+			print('你還有', x, '次機會')
+		else:
+			print('帳號已鎖定')
