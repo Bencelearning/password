dd69d3070724029659b188f17ff4a6129701e530
Bence <pscheng1203@gmail.com>
2018-10-22 00:10:52 +0800

    add .py file

---
+++ password.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+x = 3
+while True:
+	pw = input('請輸入密碼')
+	if pw == password:
+		print('登入成功')
+		break
+	else:
+		x = x -1
+		print('密碼錯誤, 你還有', x, '次機會')
+		if x == 0:
+			break
